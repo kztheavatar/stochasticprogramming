1619a8f0158e64252950f1a736c7bc2bd1fa55c5
Kevin Zhang <kztheavatar@gmail.com>
2016-09-24 14:33:56 -0500

    Initial commit HW1

---
+++ hw1/benders.py
@@ -0,0 +1,51 @@
+import cPickle
+from gurobipy import *
+
+### Read data from file you choose: commont/uncomment to choose the different files
+### This file was generated from a separate python file, using the `cPickle' module
+### This is just for convenience -- data can be read in many ways in Python
+
+dfile = open('nd1041015.pdat','r')
+#dfile = open('nd30-10-20-3000.pdat','r')
+
+Fset = cPickle.load(dfile)  # set of facilities (list of strings)
+Hset = cPickle.load(dfile)  # set of warehouses (list of strings)
+Cset = cPickle.load(dfile)  # set of customers (list of strings)
+Sset = cPickle.load(dfile)  # set of scenarios (list of strings)
+arcExpCost = cPickle.load(dfile)  # arc expansion costs (dictionary mapping F,H and H,C pairs to floats)
+facCap = cPickle.load(dfile)   # facility capacities (dictionary mapping F to floats)
+curArcCap = cPickle.load(dfile)  # current arc capacities (dictionary mapping (i,j) to floats, where either
+                                 # i is facility, j is warehouse, or i is warehouse and j is customer
+unmetCost = cPickle.load(dfile)  # penalty for unment customer demands (dicationary mapping C to floats)
+demScens = cPickle.load(dfile)  # demand scenarios (dictionary mapping (i,k) tuples to floats, where i is customer, k is
+                                #scenario
+dfile.close()
+
+### This is just a check of the data. Probably you want to comment/delete these lines once you see the structure
+
+print Fset
+print Hset
+print Cset
+print Sset
+#print arcExpCost
+print facCap
+#print curArcCap
+print unmetCost
+#print demScens
+
+
+### Define sets of arcs (used as keys to dictionaries)
+FHArcs = [(i,j) for i in Fset for j in Hset]  ## arcs from facilities to warehouses
+HCArcs = [(i,j) for i in Hset for j in Cset]   ## arcs from warehouses to customers
+AllArcs = FHArcs + HCArcs
+print AllArcs
+
+### Make them Gurobi tuplelists
+FHArcs = tuplelist(FHArcs)
+HCArcs = tuplelist(HCArcs)
+AllArcs = tuplelist(AllArcs)
+
+
+##### Start building the Model #####
+m = Model("White Russian Company")
+
